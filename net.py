aca10459646743daa08520f18081c5afb92bfa61
Lornatang <shiyipaisizuo@163.com>
2019-02-02 14:38:43 +0800

    Update net.py

--- net.py
+++ net.py
@@ -12,7 +12,7 @@ class Generator(nn.Module):
             nn.ReLU(True)
         )
         self.layer2 = nn.Sequential(
-            nn.ConvTranspose2d(64 * 4, 64 * 4, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.ConvTranspose2d(64 * 8, 64 * 4, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(64 * 4),
             nn.ReLU(True)
         )
@@ -44,27 +44,27 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2, True)
         )
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 16 * 2, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(16 * 2),
+            nn.Conv2d(64, 64 * 2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64 * 2),
             nn.LeakyReLU(0.2, True)
         )
         self.layer3 = nn.Sequential(
-            nn.Conv2d(16 * 2, 16 * 4, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(16 * 4),
+            nn.Conv2d(64 * 2, 64 * 4, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64 * 4),
             nn.LeakyReLU(0.2, True)
         )
         self.layer4 = nn.Sequential(
-            nn.Conv2d(16 * 4, 16 * 8, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(16 * 8),
+            nn.Conv2d(64 * 4, 64 * 8, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64 * 8),
             nn.LeakyReLU(0.2, True)
         )
         self.classifier = nn.Sequential(
-            nn.Conv2d(16 * 8, 1, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.Conv2d(64 * 8, 1, kernel_size=4, stride=1, padding=0, bias=False),
             nn.Sigmoid()
         )
 
