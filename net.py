5b574ea7c1d04be3c44c660625c49d82177be068
Lornatang <shiyipaisizuo@163.com>
2019-03-26 12:14:43 +0800

    Update net.py

--- net.py
+++ net.py
@@ -4,123 +4,74 @@ NOISE = 100
 
 
 class Generator(nn.Module):
-    def __init__(self, noise=NOISE):
-        super(Generator, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.ConvTranspose2d(noise, 64 * 128, kernel_size=4, stride=1, padding=0, bias=False),
-            nn.ReLU(True)
-        )
-        self.layer2 = nn.Sequential(
-            nn.ConvTranspose2d(64 * 128, 64 * 64, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64 * 64),
-            nn.ReLU(True)
-        )
-        self.layer3 = nn.Sequential(
-            nn.ConvTranspose2d(64 * 64, 64 * 32, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64 * 32),
-            nn.ReLU(True)
-        )
-        self.layer4 = nn.Sequential(
-            nn.ConvTranspose2d(64 * 32, 64 * 16, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64 * 16),
-            nn.ReLU(True)
-        )
-        self.layer5 = nn.Sequential(
-            nn.ConvTranspose2d(64 * 16, 64 * 8, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64 * 8),
-            nn.ReLU(True)
-        )
-        self.layer6 = nn.Sequential(
-            nn.ConvTranspose2d(64 * 8, 64 * 4, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64 * 4),
-            nn.ReLU(True)
-        )
-        self.layer7 = nn.Sequential(
-            nn.ConvTranspose2d(64 * 4, 64 * 2, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64 * 2),
-            nn.ReLU(True)
-        )
-        self.layer8 = nn.Sequential(
-            nn.ConvTranspose2d(64 * 2, 64, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(True)
-        )
-        self.classifier = nn.Sequential(
-            nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.Tanh()
-        )
-
-    def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.layer5(x)
-        x = self.layer6(x)
-        x = self.layer7(x)
-        x = self.layer8(x)
-        x = self.classifier(x)
-        return x
+  def __init__(self, noise=NOISE):
+    super(Generator, self).__init__()
+    self.layer1 = nn.Sequential(
+      nn.ConvTranspose2d(64 * 16, 64 * 8, kernel_size=4, stride=2, padding=1, bias=False),
+      nn.BatchNorm2d(64 * 8),
+      nn.ReLU(True)
+    )
+    self.layer2 = nn.Sequential(
+      nn.ConvTranspose2d(64 * 8, 64 * 4, kernel_size=4, stride=2, padding=1, bias=False),
+      nn.BatchNorm2d(64 * 4),
+      nn.ReLU(True)
+    )
+    self.layer3 = nn.Sequential(
+      nn.ConvTranspose2d(64 * 4, 64 * 2, kernel_size=4, stride=2, padding=1, bias=False),
+      nn.BatchNorm2d(64 * 2),
+      nn.ReLU(True)
+    )
+    self.layer4 = nn.Sequential(
+      nn.ConvTranspose2d(64 * 2, 64, kernel_size=4, stride=2, padding=1, bias=False),
+      nn.BatchNorm2d(64),
+      nn.ReLU(True)
+    )
+    self.classifier = nn.Sequential(
+      nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
+      nn.Tanh()
+    )
+  
+  def forward(self, x):
+    x = self.layer1(x)
+    x = self.layer2(x)
+    x = self.layer3(x)
+    x = self.layer4(x)
+    x = self.classifier(x)
+    return x
 
 
 class Discriminator(nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.2, True)
-        )
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(64, 64 * 2, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64 * 2),
-            nn.LeakyReLU(0.2, True)
-        )
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(64 * 2, 64 * 4, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64 * 4),
-            nn.LeakyReLU(0.2, True)
-        )
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(64 * 4, 64 * 8, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64 * 8),
-            nn.LeakyReLU(0.2, True)
-        )
-        self.layer5 = nn.Sequential(
-            nn.Conv2d(64 * 8, 64 * 16, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64 * 16),
-            nn.LeakyReLU(0.2, True)
-        )
-        self.layer6 = nn.Sequential(
-            nn.Conv2d(64 * 16, 64 * 32, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64 * 32),
-            nn.LeakyReLU(0.2, True)
-        )
-        self.layer7 = nn.Sequential(
-            nn.Conv2d(64 * 32, 64 * 64, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64 * 64),
-            nn.LeakyReLU(0.2, True)
-        )
-        self.layer8 = nn.Sequential(
-            nn.Conv2d(64 * 64, 64 * 128, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64 * 128),
-            nn.LeakyReLU(0.2, True)
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Conv2d(64 * 128, 1, kernel_size=4, stride=1, padding=0, bias=False),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.layer5(x)
-        x = self.layer6(x)
-        x = self.layer7(x)
-        x = self.layer8(x)
-        x = self.classifier(x)
-        out = x.view(-1, 1).squeeze(1)
-        return out
+  def __init__(self):
+    super(Discriminator, self).__init__()
+    self.layer1 = nn.Sequential(
+      nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
+      nn.LeakyReLU(0.2, True)
+    )
+    self.layer2 = nn.Sequential(
+      nn.Conv2d(64, 64 * 2, kernel_size=4, stride=2, padding=1, bias=False),
+      nn.BatchNorm2d(64 * 2),
+      nn.LeakyReLU(0.2, True)
+    )
+    self.layer3 = nn.Sequential(
+      nn.Conv2d(64 * 2, 64 * 4, kernel_size=4, stride=2, padding=1, bias=False),
+      nn.BatchNorm2d(64 * 4),
+      nn.LeakyReLU(0.2, True)
+    )
+    self.layer4 = nn.Sequential(
+      nn.Conv2d(64 * 4, 64 * 8, kernel_size=4, stride=2, padding=1, bias=False),
+      nn.BatchNorm2d(64 * 8),
+      nn.LeakyReLU(0.2, True)
+    )
+    self.classifier = nn.Sequential(
+      nn.Conv2d(64 * 8, 1, kernel_size=4, stride=1, padding=0, bias=False),
+      nn.Sigmoid()
+    )
+  
+  def forward(self, x):
+    x = self.layer1(x)
+    x = self.layer2(x)
+    x = self.layer3(x)
+    x = self.layer4(x)
+    x = self.classifier(x)
+    out = x.view(-1, 1).squeeze(1)
+    return out
